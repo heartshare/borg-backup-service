7f7e0cf19e73def5a053eb3bc46644e54440d045
Mohammed Ajil <mohammed@ajil.ch>
2020-06-15 08:52:34 +0200

    remove unnecessary lines

--- backup_script.sh
+++ backup_script.sh
@@ -1,6 +1,4 @@
-#BORG_PASSPHRASE=9Kz86kMJNP586wdrsnGGntLQoPZVjJ9pLhjQBFBjh92EE9RHcH borg create --exclude='/home/srv-user/volumes/plex/library/Library/Application Support/Plex Media Server/Cache/*' --exclude='*.domains' --exclude='/home/srv-user/volumes/*/downloads/*' --stats --progress ssh://srv-user@phyrat-nas.synology.me:22223/backup::'{hostname}-{now}' /home
 echo "Creating new backup..."
-borg create ${BORG_OPTIONS} --verbose --stats ssh://${BORG_USER}@${BORG_HOST}:${BORG_PORT}/${BORG_REPO_PATH_ON_HOST}::${ARCHIVE_NAME}'-'$(date +'%Y-%m-%d-%H:%M') /backup_source
 
 if borg create ${BORG_OPTIONS} --stats ssh://${BORG_USER}@${BORG_HOST}:${BORG_PORT}/${BORG_REPO_PATH_ON_HOST}::${ARCHIVE_NAME}'-'$(date +'%Y-%m-%d-%H:%M') /backup_source ; then
   echo "Backup succeeded. Pruning..."
