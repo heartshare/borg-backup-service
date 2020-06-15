660bc37a1632674bb902e9f5279e1e32a099e748
Mohammed Ajil <mohammed@ajil.ch>
2020-06-15 08:54:14 +0200

    parametrize pruning

--- backup_script.sh
+++ backup_script.sh
@@ -2,7 +2,7 @@ echo "Creating new backup..."
 
 if borg create ${BORG_OPTIONS} --stats ssh://${BORG_USER}@${BORG_HOST}:${BORG_PORT}/${BORG_REPO_PATH_ON_HOST}::${ARCHIVE_NAME}'-'$(date +'%Y-%m-%d-%H:%M') /backup_source ; then
   echo "Backup succeeded. Pruning..."
-  borg prune --keep-hourly 6 --keep-daily 7 --keep-weekly 4
+  borg prune --keep-hourly ${HOURLY_BACKUPS} --keep-daily ${DAILY_BACKUPS} --keep-weekly ${WEEKLY_BACKUPS}
   echo "Backup finished."
 else
   curl -X POST -H 'Content-type: application/json' --data '{"text":"There was a problem with the backup!"}' ${SLACK_WEBHOOK}
